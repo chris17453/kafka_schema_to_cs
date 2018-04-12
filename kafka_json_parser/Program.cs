797f1490d9f79244c09f32b35abe0c962c1c1eb6
Chris Watkins <charles.watkins@insightglobal.com>
2018-04-11 20:10:08 -0400

    Initial Commit

---
+++ kafka_json_parser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+namespace kafka_json_parser{
+    class Program{
+        static void Main(string[] args) {
+            string [] file_list=files.get();
+            //foreach(string file in file_list) {
+            string file=@"C:\repos\atlis-json-schema\account-management\event\AccountCreated.v1.json";
+            //string file=@"C:\repos\atlis-json-schema\event.v1.json";
+                Task taskA = Task.Factory.StartNew(() => parse.jsonAsync(file) );
+                taskA.Wait();
+            //}
+            Console.ReadKey();
+        }//end main
+    }//end class
+}//end namespace 
---
+++ kafka_json_parser/class/load_files.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kafka_json_parser{
+    public static class files{
+
+        static string source_dir= @"C:\repos\atlis-json-schema\";
+        static string file_type = "*.json";
+        
+        public static string[] get(){
+             string[] files = Directory.GetFiles(source_dir,file_type,SearchOption.AllDirectories);
+            return files;
+        }//end get_files
+
+
+    }
+}
